66ad20f5aff0db33f6b4f36274bdb1d5fd3bfff2
Dale Payne <dalebird88@gmail.com>
2016-06-03 20:12:51 -0600

    buttons working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
 angular.module("app",['ui.router']);
+
+angular.module("app").config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+        .state('home', {
+            url: '/',
+            templateUrl: '../routes/home.html'
+        })
+
+        .state('red', {
+            url:"/red",
+            templateUrl:"../routes/colors/red.html"
+        })
+        .state('orange', {
+            url:"/orange",
+            templateUrl:"../routes/colors/orange.html"
+        })
+        .state('yellow', {
+            url:"/yellow",
+            templateUrl:"../routes/colors/yellow.html"
+        })
+        .state('green', {
+            url:"/green",
+            templateUrl:"../routes/colors/green.html"
+        })
+        .state('blue', {
+            url:"/blue",
+            templateUrl:"../routes/colors/blue.html"
+        })
+        .state('indigo', {
+            url:"/indigo",
+            templateUrl:"../routes/colors/indigo.html"
+        })
+        .state('violet', {
+            url:"/violet",
+            templateUrl:"../routes/colors/violet.html"
+        })
+        .state('black', {
+            url:"/black",
+            templateUrl:"../routes/colors/black.html"
+        })
+});
