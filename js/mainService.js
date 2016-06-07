251b9a814d720bbfad9da3b59f94949b2336d149
Dale Payne <dalebird88@gmail.com>
2016-06-07 10:12:05 -0600

    images loaded

--- js/mainService.js
+++ js/mainService.js
@@ -3,30 +3,56 @@ angular.module("app")
 
   var red = [
     "https://i.ytimg.com/vi/qZMfouM2sH4/maxresdefault.jpg",
-    "http://1.bp.blogspot.com/-krw0fzyHEuQ/T0T_pkJIyQI/AAAAAAAAAx0/InxfKI0ddcM/s1600/One_Piece_Title_9.PNG"
+    "http://1.bp.blogspot.com/-krw0fzyHEuQ/T0T_pkJIyQI/AAAAAAAAAx0/InxfKI0ddcM/s1600/One_Piece_Title_9.PNG",
+    "https://kakeracomplex.files.wordpress.com/2011/01/baccano1.jpg",
+    "http://vignette3.wikia.nocookie.net/toonami/images/4/4f/Qyzm02.jpg/revision/latest?cb=20130102093559",
+    "http://cdn2.artofthetitle.com/assets/sm/upload/6n/on/ii/gy/cowboy_bebop_t.jpg?k=1e8fc07403"
+
   ]
 
   var orange = [
     "https://static.hummingbird.me/gallery_images/images/000/000/305/original/305.jpg?1375598156",
     "http://is4.mzstatic.com/image/thumb/Music6/v4/f6/b6/d1/f6b6d1b6-2132-10d1-72d4-c823a7ee4969/source/400x400-75.jpg",
-    "http://my-anime-music.com/wp-content/uploads/2011/03/lucky-star.jpg"
+    "http://my-anime-music.com/wp-content/uploads/2011/03/lucky-star.jpg",
+    "https://www.madman.com.au/news/wp-content/uploads/2011/04/kon.jpg",
+    "https://images7.alphacoders.com/332/332295.jpg"
   ];
 
   var yellow = [
-    // log horizon death parade
+    "http://www.entravity.com/wp-content/uploads/2014/03/Log-Horizon-Anime-Wallpaper.jpg",
+    "http://ib1.huluim.com/show/21464?region=US&size=952x536",
+    "http://supervillainnetwork.com/wp-content/uploads/2015/04/ghost-in-the-shell-0.jpg",
+    "http://goldenarmmedia.com/wp-content/uploads/2015/07/neon-genesis-evangelion.jpg",
+    "http://static.zerochan.net/Re-l.Mayer.full.160192.jpg"
   ]
 
   var green = [
-    //
+    "http://afistfulofculture.com/wp-content/uploads/2012/12/1477-series-header.jpg",
+    "https://marblesforpolish.files.wordpress.com/2014/07/1478-series-header.jpg",
+    "http://orig03.deviantart.net/129b/f/2009/025/c/0/spirited_away_opening_title_by_shad_wolf.jpg",
+    "https://thebookwars.files.wordpress.com/2014/06/thewindrises.jpg",
+    "http://4.bp.blogspot.com/-pi4WqjbTJzQ/UWktcN7453I/AAAAAAAARuA/mdngxp1JPj0/s1600/castle_in_the_sky_wallpaper_by_swfan1977-d32r1f5.jpg"
   ]
   var blue = [
-    //princess tutu?
+    "http://cdn.shopify.com/s/files/1/0228/9021/products/RWBY_Pocket_800_Art.png?v=1463156024",
+    "http://vignette2.wikia.nocookie.net/princesstutu/images/b/b5/Banner.jpg/revision/latest?cb=20130726213839",
+    "https://uploads.disquscdn.com/images/6115cfe3138b523647de9f2ffe99d562047b1dc78758bb1a09ee62bada6bbb05.jpg",
+    "https://s-media-cache-ak0.pinimg.com/736x/60/5a/e4/605ae48922873f36c22b54cbaf294ad7.jpg",
+    "http://img1.ak.crunchyroll.com/i/spire4/b9899d2d541ebb802d1d2b5a091460511309282622_fwide.jpg"
   ]
   var indigo = [
-    //
+    "http://cdn.paper4pc.com/images/clannad-wallpaper-6.jpg",
+    "https://soloordoppio.files.wordpress.com/2011/12/toradora_love_by_inslock.jpg",
+    "http://www.garytay.com/eleventsuki/wp-content/uploads/2015/12/Ore-Monogatari.png",
+    "http://assets.huluim.com/shows/key_art_fruits_basket.jpg",
+    "http://when-will.net/images/artikel/2015/march/Wolf-Girl-and-Black-Prince.jpg"
   ]
   var black = [
-    // attack on titan death note black lagoon
+    "http://actionagogo.com.previewdns.com/wp-content/uploads/2014/11/aot.jpg",
+    "http://cdn.playbuzz.com/cdn/48129886-6746-44ed-868c-245be9bb2ec1/6d864acd-8afb-45fc-a161-7b1deb839ae0.jpg",
+    "http://vignette1.wikia.nocookie.net/lagooncompany/images/b/b1/Black_Lagoon.jpg/revision/latest?cb=20110718042001",
+    "http://blog.neo-amusement.com/wp-content/uploads/2011/02/berserk.59919.jpg",
+    "http://www.absoluteanime.com/hellsing_ultimate/wallpaper/hellsing4_1280.jpg"
   ]
 
 this.getcolor = function(arr){
